ae53d8ce07eb7676ecd8f20208ed5228477d9ff5
vardhman03 <vardhmanjain2003@gmail.com>
2024-04-14 19:17:17 +0530

    Conversionofdatatypes

---
+++ vscodde/learning js/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//newer version of javascript
+//alert(3+3)//this is nodejs not browser
+console.log(3+3)//code redability should be high
+console.log("vj") 
+
+let name="jfakl"
+let age=19
+let islogin=true
+
+//number=>2 to power 53
+//bigint
+//string =>""
+//boolean=>true\false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
---
+++ vscodde/learning js/03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score="true"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)
+
+//"33"=>33
+//"78fs"=>NaN
+//true=>1 false=>0
+
+let isLoggedIn="fhg"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true; 0=>false
+//""=>false
+//"vae"=>true
+
+let someno=4452
+let strnum=String(someno)
+console.log(someno)
+console.log(typeof strnum)
