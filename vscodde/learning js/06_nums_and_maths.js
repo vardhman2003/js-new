2e6fb9e59c9d1461704d72d7bf005c84b1534275
vardhman03 <vardhmanjain2003@gmail.com>
2024-05-06 23:59:32 +0530

    nums and  maths

---
+++ vscodde/learning js/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+const sc=50
+// console.log(sc)
+
+const bal = new Number (340)
+// console.log(bal);
+
+// console.log(bal.toString().length)
+// console.log(bal.toFixed(2))
+
+const otn=48.47654
+// console.log(otn.toPrecision(4));
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString())
+
+//++++++++++++++++Maths+++++++++++++++++++++++
+// console.log(Math)
+// console.log(Math.abs(-459))
+// console.log(Math.round(6.49));
+// console.log(Math.ceil(6.49));
+// console.log(Math.floor(6.49));
+// console.log(Math.sqrt(9));
+// console.log(Math.max(4,52,8,96,2,4,6,5));
+
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log((Math.random()*10)+1);
+
+const max=20
+const min=10
+console.log(Math.floor(Math.random()*(max-min+1))+min)
