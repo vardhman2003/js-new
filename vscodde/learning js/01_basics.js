77023549f70ab6a6ba5a224257d06d65b050a9e9
vardhman03 <vardhmanjain2003@gmail.com>
2024-05-06 23:56:37 +0530

    basics

---
+++ vscodde/learning js/01_basics.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="vardhmanjain2003@gmail.com"
+var accountPassword="96625654"
+accountCity="katni"
+
+// accountId=15//not allowed
+console.log(accountId);
+accountEmail="vardhmanjain0311@gmail.com"
+console.log(accountEmail)
+accountPassword="25654"
+accountCity="JBP"
+/*Prefer not to use var due to issue of block scope and functional scope. */
+console.table([accountId,accountEmail,accountPassword,accountCity ])
