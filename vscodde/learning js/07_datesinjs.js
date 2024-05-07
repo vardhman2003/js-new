046493319436c02614b47b09f50af4d2b0744de1
vardhman03 <vardhmanjain2003@gmail.com>
2024-05-07 17:07:34 +0530

    date and time

---
+++ vscodde/learning js/07_datesinjs.js
@@ -0,0 +1,30 @@
+// Dates/
+
+let myDate= new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
+
+// let myCreatedDate=new Date(2023,0, 23)
+// let myCreatedDate=new Date(2023,0, 23,12,6,4)
+// let myCreatedDate=new Date("2024-05-07")
+let myCreatedDate=new Date("02-29-2024")
+console.log(myCreatedDate.toDateString());
+
+let myTimeStamp= Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000))/
+
+let newDate=new Date()
+// console.log(newDate);/
+// console.log(newDate.getMinutes());
+
+// newDate.toLocaleString('default',{
+//     weekday:"narrow"
+// })
